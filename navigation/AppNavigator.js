edea5541f07244c56f5f9769ea2e2c84a687c1fc
lmarioni <lucasmarionilm@gmail.com>
2019-02-08 12:22:10 -0300

    Modularizo pantalla login + AuthLogin

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,69 +2,9 @@ import React from 'react';
 import { Button, Text, View, StyleSheet,  ActivityIndicator, AsyncStorage, StatusBar } from 'react-native';
 import { createAppContainer, createSwitchNavigator, createStackNavigator } from 'react-navigation';
 
-import MainTabNavigator from './MainTabNavigator';
-
-
-
-
-class SignInScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Please sign in',
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Button title="Sign in!" onPress={this._signInAsync} />
-      </View>
-    );
-  }
-
-  _signInAsync = async () => {
-    await AsyncStorage.setItem('userToken', 'abc');
-    this.props.navigation.navigate('App');
-  };
-}
-
-class AuthLoadingScreen extends React.Component {
-  constructor() {
-    super();
-    this._bootstrapAsync();
-   
-  }
-
-  // Fetch the token from storage then navigate to our appropriate place
-  _bootstrapAsync = async () => {
-    const userToken = await AsyncStorage.getItem('userToken');
-
-    // This will switch to the App screen or Auth screen and this loading
-    // screen will be unmounted and thrown away.
-    // console.log('token'+userToken);
-
-    this.props.navigation.navigate(userToken ? 'App' : 'Auth');
-  };
-
-  // Render any loading content that you like here
-  render() {
-    return (
-      <View style={styles.container}>
-        <ActivityIndicator />
-        <StatusBar barStyle="default" />
-      </View>
-    );
-  }
-}
-
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
+import AuthLoadingScreen from '../screens/AuthLoadingScreen';//valida si ya esta logueado o no
+import SignInScreen from '../screens/SinginScreen';//pantalla login
+import MainTabNavigator from './MainTabNavigator';//es el navegador de la app cuando estas logeado
 
 const AuthStack = createStackNavigator({ SignIn: SignInScreen });
 
