3b7f9078285f6bc92ef92c9844fcfdd43cd2179e
lmarioni <lucasmarionilm@gmail.com>
2019-02-08 11:19:57 -0300

    Base
    acá todo anda divino, esta acomodado por login, despues se va a la applicacion... Ahora hay que empezar a modularizar mas

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { Button, Text, View, StyleSheet,  ActivityIndicator, AsyncStorage, StatusBar } from 'react-native';
+import { createAppContainer, createSwitchNavigator, createStackNavigator } from 'react-navigation';
+
+import MainTabNavigator from './MainTabNavigator';
+
+
+
+
+class SignInScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Please sign in',
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button title="Sign in!" onPress={this._signInAsync} />
+      </View>
+    );
+  }
+
+  _signInAsync = async () => {
+    await AsyncStorage.setItem('userToken', 'abc');
+    this.props.navigation.navigate('App');
+  };
+}
+
+class AuthLoadingScreen extends React.Component {
+  constructor() {
+    super();
+    this._bootstrapAsync();
+   
+  }
+
+  // Fetch the token from storage then navigate to our appropriate place
+  _bootstrapAsync = async () => {
+    const userToken = await AsyncStorage.getItem('userToken');
+
+    // This will switch to the App screen or Auth screen and this loading
+    // screen will be unmounted and thrown away.
+    // console.log('token'+userToken);
+
+    this.props.navigation.navigate(userToken ? 'App' : 'Auth');
+  };
+
+  // Render any loading content that you like here
+  render() {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator />
+        <StatusBar barStyle="default" />
+      </View>
+    );
+  }
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
+const AuthStack = createStackNavigator({ SignIn: SignInScreen });
+
+export default createAppContainer(createSwitchNavigator({
+  // You could add another route here for authentication.
+  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+  AuthLoading: AuthLoadingScreen,
+  Auth: AuthStack,
+  App: MainTabNavigator,
+},{
+  initialRouteName: 'AuthLoading',
+}));
+
